e09fe9ded7065bf3f2286348cb466091c4105b94
Alram Lechner <alram.lechner@gmx.at>
2021-12-04 21:41:04 +0100

    Speziellen Influx-Write node für Stromzähler eingefügt

---
+++ InfluxDbNode/InfluxWriterHelper.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+
+namespace alram_lechner_gmx_at.logic.InfluxDb2
+{
+    class InfluxWriterHelper
+    {
+
+        public static void WriteDatapointSync(String influxUrl, String measureName, String measureTags, String fieldName, double value, Action<int?, string> SetResultCallback)
+        {
+            UriBuilder uriBuilder = null;
+            try
+            {
+                uriBuilder = new UriBuilder(influxUrl);
+            } catch (UriFormatException e)
+            {
+                SetResultCallback(997, e.Message + "; URI was " + influxUrl + "; tags: " + measureTags);
+                return;
+            }
+            if (uriBuilder.Port == -1)
+            {
+                uriBuilder.Port = 8086;
+            }
+            String queryToAppend = "precision=s";
+            if (uriBuilder.Query != null && uriBuilder.Query.Length > 1)
+                uriBuilder.Query = uriBuilder.Query.Substring(1) + "&" + queryToAppend;
+            else
+                uriBuilder.Query = queryToAppend;
+
+            // Open HTTP connection:
+            String Body = "";
+            try
+            {
+                HttpWebRequest client = (HttpWebRequest)HttpWebRequest.Create(uriBuilder.Uri);
+                client.Method = "POST";
+                client.ContentType = "text/plain";
+
+                using (var request = client.GetRequestStream())
+                {
+                    using (var writer = new StreamWriter(request))
+                    {
+                        Body = measureName;
+                        if (measureTags != null && measureTags.Length > 0)
+                        {
+                            Body += "," + measureTags;
+                        }
+
+                        Body += " ";
+                        Body += fieldName + "=" + value.ToString("G", CultureInfo.InvariantCulture);
+
+                        writer.Write(Body);
+                    }
+                }
+                var response = client.GetResponse();
+                using (var result = response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(result))
+                    {
+                        SetResultCallback(null, null);
+                    }
+                }
+            }
+            catch (WebException e)
+            {
+                if (e.Response is HttpWebResponse errorResponse)
+                {
+                    try
+                    {
+                        using (var result = errorResponse.GetResponseStream())
+                        {
+                            using (var reader = new StreamReader(result))
+                            {
+                                SetResultCallback((int)errorResponse.StatusCode, reader.ReadToEnd() + "; Line was: " + Body);
+                                return;
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                    }
+                }
+                SetResultCallback(998, "Unknown error" + "; Line was: " + Body);
+            }
+            catch (Exception e)
+            {
+                SetResultCallback(999, e.Message + "; Line was: " + Body);
+                return;
+            }
+        }
+    }
+}
---
+++ InfluxDbNode/WriteElectricMeter.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Threading;
+using LogicModule.Nodes.Helpers;
+using LogicModule.ObjectModel;
+using LogicModule.ObjectModel.TypeSystem;
+using System.Globalization;
+
+namespace alram_lechner_gmx_at.logic.InfluxDb2
+{
+
+    public class WriteElectricMeter : LogicNodeBase
+    {
+        // Parameter
+        [Parameter(DisplayOrder = 1, IsRequired = true, IsDefaultShown = false)]
+        public StringValueObject InfluxDbUrl { get; private set; }
+
+        [Parameter(DisplayOrder = 2, IsRequired = true, IsDefaultShown = true)]
+        public StringValueObject InfluxMeasureName { get; private set; }
+
+        [Parameter(DisplayOrder = 3, IsRequired = true, IsDefaultShown = true)]
+        public StringValueObject InfluxMeasureTags { get; private set; }
+        
+        // Input
+        [Input(DisplayOrder = 7, IsInput = true, IsRequired = true)]
+        public DoubleValueObject MainMeterValue { get; private set; }
+
+        [Input(DisplayOrder = 8, IsInput = true, IsRequired = true)]
+        public DoubleValueObject CurrentPowerValue { get; private set; }
+
+        [Input(DisplayOrder = 9, IsInput = true, IsRequired = true)]
+        public DoubleValueObject DailyMeterValue { get; private set; }
+
+        // Output
+        [Output(DisplayOrder = 2, IsRequired = false, IsDefaultShown = false)]
+        public BoolValueObject ResetDailyMeterCounter { get; private set; }
+
+        [Output(DisplayOrder = 3, IsRequired = false, IsDefaultShown = false)]
+        public IntValueObject ErrorCode { get; private set; }
+
+        [Output(DisplayOrder = 4, IsRequired = false, IsDefaultShown = false)]
+        public StringValueObject ErrorMessage { get; private set; }
+
+        private ITypeService TypeService = null;
+
+        public WriteElectricMeter(INodeContext context) : base(context)
+        {
+            context.ThrowIfNull("context");
+            this.TypeService = context.GetService<ITypeService>();
+            this.InfluxDbUrl = TypeService.CreateString(PortTypes.String, "Influx DB URL", "http://<hostname>:<port>/write?db=<database>");
+            this.InfluxMeasureName = TypeService.CreateString(PortTypes.String, "Measure name", "sensor");
+            this.InfluxMeasureTags = TypeService.CreateString(PortTypes.String, "Tags", "room=kitchen,device=washingmachine");
+            this.MainMeterValue = TypeService.CreateDouble(PortTypes.Number, "Main counter (kWh)");
+            this.CurrentPowerValue = TypeService.CreateDouble(PortTypes.Number, "Current power (W)");
+            this.DailyMeterValue = TypeService.CreateDouble(PortTypes.Number, "Daily counter (Wh)");
+            this.ResetDailyMeterCounter = TypeService.CreateBool(PortTypes.Bool, "Reset daily counter", false);
+            this.ErrorCode = TypeService.CreateInt(PortTypes.Integer, "HTTP status-code");
+            this.ErrorMessage = TypeService.CreateString(PortTypes.String, "Error message");
+        }
+
+        public override void Startup()
+        {
+        }
+
+        public override ValidationResult Validate(string language)
+        {
+            return base.Validate(language);
+        }
+
+        public override void Execute()
+        {
+            if (!InfluxDbUrl.HasValue || !InfluxMeasureName.HasValue)
+            {
+                return;
+            }
+
+            if (this.CurrentPowerValue.HasValue && this.CurrentPowerValue.WasSet)
+            {
+                WriteDatapointAsync("power", this.CurrentPowerValue.Value);
+            }
+
+            if (this.MainMeterValue.HasValue && this.MainMeterValue.WasSet)
+            {
+                WriteDatapointAsync("meter", this.MainMeterValue.Value);
+            }
+
+            if (this.DailyMeterValue.HasValue && this.DailyMeterValue.WasSet)
+            {
+                WriteDatapointAsync("intermediatecounter", this.DailyMeterValue.Value);
+                if (this.DailyMeterValue.Value > 0)
+                {
+                    this.ResetDailyMeterCounter.Value = true;
+                }
+            }
+
+        }
+
+        public void WriteDatapointAsync(String fieldName, double value)
+        {
+            var thread = new Thread(() =>
+            {
+                InfluxWriterHelper.WriteDatapointSync(
+                    this.InfluxDbUrl.Value,
+                    this.InfluxMeasureName.Value,
+                    this.InfluxMeasureTags.HasValue ? this.InfluxMeasureTags.Value : null,
+                    fieldName,
+                    value,
+                    (errorCode, errorMessage) =>
+                    {
+                        if (errorCode != null)
+                        {
+                            ErrorCode.Value = errorCode.Value;
+                        }
+                        if (errorMessage != null)
+                        {
+                            ErrorMessage.Value = errorMessage;
+                        }
+                    });
+            });
+            thread.Start();
+        }
+    }
+
+}
